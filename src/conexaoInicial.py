90686ab207840fe61f632c216afbfe8bb055d5c7
HenriqueManfrin <henriquemanfrin@gmail.com>
2026-02-01 23:15:53 -0300

    looping em conversa com IA

--- src/conexaoInicial.py
+++ src/conexaoInicial.py
@@ -1,12 +1,17 @@
 import ollama
+def conversacao():
+    while True:
+        MensagemInicial = input('\n'"Escreva aqui a sua mensagem: ")
+        if MensagemInicial == "sair":
+            exit()
 
-MensagemInicial = input("Escreva aqui a sua mensagem: ")
+        stream = ollama.chat(
+            model='gemma:latest',
+            messages=[{'role': 'user', 'content': MensagemInicial}],
+            stream=True, 
+        )
 
-stream = ollama.chat(
-    model='gemma:latest',
-    messages=[{'role': 'user', 'content': MensagemInicial}],
-    stream=True, 
-)
+        for chunk in stream:
+            print(chunk['message']['content'], end='', flush=True)
 
-for chunk in stream:
-    print(chunk['message']['content'], end='', flush=True)
+conversacao = conversacao()
